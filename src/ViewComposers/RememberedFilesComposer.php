8b211516d9d286231baf8a531d747d6e6eb302d3
Simon Dann <carbontwelve@users.noreply.github.com>
2017-10-24 12:50:15 +0100

    Update RememberedFilesComposer.php

--- src/ViewComposers/RememberedFilesComposer.php
+++ src/ViewComposers/RememberedFilesComposer.php
@@ -7,15 +7,16 @@ use App\Repositories\UserRepository;
 
 class RememberedFilesComposer
 {
+    private $session;
 
     /**
      * Create a new remembered files composer.
      *
-     * @return void
+     * @param \Illuminate\Session\Store $store
      */
-    public function __construct()
+    public function __construct(\Illuminate\Session\Store $store)
     {
-        // 
+        $this->session = $store;
     }
 
     /**
@@ -26,6 +27,14 @@ class RememberedFilesComposer
      */
     public function compose(View $view)
     {
-        // 
+        $fileBag = new FileBag();
+
+        if ($files = $this->session->get('remembered.files', null)) {
+            foreach($files as $key => $file) {
+                $fileBag->set($key, new UploadedFile($file['tmpPathName'], $file['originalName']));
+            }
+        }
+
+        $view->with('rememberedFiles', $fileBag);
     }
 }
