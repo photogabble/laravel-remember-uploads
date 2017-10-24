c17a51cc6fdf2034a37ba43da2aea0901cc8b70c
Simon Dann <carbontwelve@users.noreply.github.com>
2017-10-24 12:24:20 +0100

    Create RememberedFilesComposer.php

---
+++ src/ViewComposers/RememberedFilesComposer.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Photogabble\LaravelRememberUploads\ViewComposers;
+
+use Illuminate\View\View;
+use App\Repositories\UserRepository;
+
+class RememberedFilesComposer
+{
+
+    /**
+     * Create a new remembered files composer.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        // 
+    }
+
+    /**
+     * Bind data to the view.
+     *
+     * @param  View  $view
+     * @return void
+     */
+    public function compose(View $view)
+    {
+        // 
+    }
+}
