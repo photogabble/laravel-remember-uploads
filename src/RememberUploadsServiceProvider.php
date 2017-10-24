9d020eb46c505d7e018e4acf474abc2e3c29aeea
Simon Dann <carbontwelve@users.noreply.github.com>
2017-10-24 12:27:22 +0100

    Create RememberUploadsServiceProvider.php

---
+++ src/RememberUploadsServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Photogabble\LaravelRememberUploads;
+
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
+
+class ComposerServiceProvider extends ServiceProvider
+{
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Using class based composers...
+        View::composer(
+            'Photogabble\LaravelRememberUploads\ViewComposers\RememberedFilesComposer'
+        );
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
