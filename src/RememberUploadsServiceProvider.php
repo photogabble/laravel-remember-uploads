12f3e8e3d088d8a2434fa140064a4a22806d673d
Simon Dann <carbontwelve@users.noreply.github.com>
2017-10-25 09:33:54 +0100

    Update RememberUploadsServiceProvider.php

--- src/RememberUploadsServiceProvider.php
+++ src/RememberUploadsServiceProvider.php
@@ -16,6 +16,7 @@ class ComposerServiceProvider extends ServiceProvider
     {
         // Using class based composers...
         View::composer(
+            '*',
             'Photogabble\LaravelRememberUploads\ViewComposers\RememberedFilesComposer'
         );
     }
