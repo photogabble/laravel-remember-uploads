f50e7a7eca30158540cd15ec30e363033f2c2d0b
Simon Dann <carbontwelve@users.noreply.github.com>
2018-01-14 14:25:44 +0000

    :construction: use laravels UploadFile class

--- src/helpers.php
+++ src/helpers.php
@@ -4,7 +4,7 @@ if (! function_exists('oldFile')) {
     /**
      * @param null|string $key
      * @param null|mixed $default
-     * @return mixed|\Symfony\Component\HttpFoundation\FileBag|Symfony\Component\HttpFoundation\File\UploadedFile
+     * @return mixed|\Symfony\Component\HttpFoundation\FileBag|Illuminate\Http\UploadedFile
      */
     function oldFile($key = null, $default = null) {
         /** @var Illuminate\Session\Store $session */
@@ -14,7 +14,7 @@ if (! function_exists('oldFile')) {
             foreach($files as $k => $f) {
                 $fileBag->set(
                     $k,
-                    new Symfony\Component\HttpFoundation\File\UploadedFile(
+                    new Illuminate\Http\UploadedFile(
                         $f['tmpPathName'],
                         $f['originalName'],
                         $f['mimeType'],
